ea892204b9eed55efec5d8ac0bc61e454bc3ffb6
pilming44 <pilming44@naver.com>
2023-02-13 22:42:23 +0900

    초기세팅 커밋

---
+++ src/main/java/study/toy/everythingshop/EverythingShopApplication.java
@@ -0,0 +1,13 @@
+package study.toy.everythingshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EverythingShopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EverythingShopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/study/toy/everythingshop/EverythingShopApplicationTests.java
@@ -0,0 +1,13 @@
+package study.toy.everythingshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EverythingShopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
