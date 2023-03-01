509457c6af18e8b9fd595f5c6d8ea08a834929bc
pilming44 <pilming44@naver.com>
2023-03-01 23:22:00 +0900

    feat: ProductMEntity생성, ProductSearchDTO생성

--- src/main/java/study/toy/everythingshop/entity/ProductMEntity.java
+++ src/main/java/study/toy/everythingshop/entity/ProductMEntity.java
@@ -1,8 +1,14 @@
 package study.toy.everythingshop.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@Builder //상속받는 클래스가 없으므로 SuperBuilder가 아닌 builder사용
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductMEntity {
     private Long productNum;
     private Long userNum;
