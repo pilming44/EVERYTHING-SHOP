fb09ce9136b345d3e66ea26a97389c0a9c7ab350
pilming44 <pilming44@naver.com>
2023-03-21 00:36:30 +0900

    faet: 머지 오류 수정

--- src/main/java/study/toy/everythingshop/repository/impl/MemberDAOImpl.java
+++ src/main/java/study/toy/everythingshop/repository/impl/MemberDAOImpl.java
@@ -1,15 +1,19 @@
 package study.toy.everythingshop.repository.impl;
 
+import lombok.RequiredArgsConstructor;
 import org.apache.ibatis.session.SqlSession;
+import org.springframework.stereotype.Repository;
 import study.toy.everythingshop.entity.UserMEntity;
 import study.toy.everythingshop.repository.MemberDAO;
 
 import java.util.List;
 import java.util.Optional;
 
+@Repository
+@RequiredArgsConstructor
 public class MemberDAOImpl implements MemberDAO {
 
-    private SqlSession sqlSession;
+    private final SqlSession sqlSession;
     @Override
     public int join(UserMEntity userMEntity) {
         return sqlSession.insert("study.toy.everythingshop.repository.MemberDAO.join", userMEntity);
