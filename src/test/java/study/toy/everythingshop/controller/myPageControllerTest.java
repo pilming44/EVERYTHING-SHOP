12210c36ffe91f62a5cae86255603003d0ad61b4
pilming44 <pilming44@naver.com>
2023-04-02 13:50:51 +0900

    test: 개인정보 수정 테스트 코드 추가

--- src/test/java/study/toy/everythingshop/controller/myPageControllerTest.java
+++ src/test/java/study/toy/everythingshop/controller/myPageControllerTest.java
@@ -7,9 +7,14 @@ import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMock
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.transaction.annotation.Transactional;
+import study.toy.everythingshop.entity.UserMEntity;
+import study.toy.everythingshop.repository.UserDAO;
 import study.toy.everythingshop.service.UserService;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.hamcrest.Matchers.hasProperty;
 import static org.hamcrest.Matchers.is;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
@@ -27,9 +32,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class myPageControllerTest {
     @Autowired
     MockMvc mockMvc;
-
     @Autowired
     UserService userService;
+    @Autowired
+    UserDAO userDAO;
 
     @Test
     @DisplayName("비로그인 마이페이지 접근시 리다이렉트")
@@ -61,9 +67,51 @@ public class myPageControllerTest {
     }
 
     @Test
-    @DisplayName("로그인 사용자 내정보 수정")
+    @DisplayName("로그인 사용자 내정보 수정 - 성공")
     @WithUser(value = "admin") //@BeforeEach 와 @WithUserDetails 사용시 오류로 인해 커스텀 태그 생성
     void test_4() throws Exception{
         //todo
+        String userId = "admin";
+        String newUserName = "변경이름";
+
+        //이름 수정 post요청
+        mockMvc.perform(MockMvcRequestBuilders.post("/myPage/editMyInfo/{userId}", userId)
+                        .param("userId", userId)
+                        .param("userNm", newUserName))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(MockMvcResultMatchers.redirectedUrl("/myPage/myInfo"));
+
+        //검증
+        UserMEntity userMEntity = userDAO.findByUserId(userId);
+        assertThat(newUserName).isEqualTo(userMEntity.getUserNm());
+    }
+
+    @Test
+    @DisplayName("로그인 사용자 내정보 수정 - 실패(변조시도)")
+    @WithUser(value = "admin") //@BeforeEach 와 @WithUserDetails 사용시 오류로 인해 커스텀 태그 생성
+    void test_5() throws Exception{
+        String userId = "admin2"; //본인의 아이디가 아닌 다른아이디로 변조해서 시도 시
+        String newUserName = "변경이름";
+
+        //2023-04-02 현재는 예외에대한 처리가 없어서 Forbidden처리. 예외처리에대한 방법이 생기면 변경 필요
+        mockMvc.perform(MockMvcRequestBuilders.post("/myPage/editMyInfo/{userId}", userId)
+                        .param("userId", userId)
+                        .param("userNm", newUserName))
+                .andExpect(status().isForbidden());
+    }
+
+    @Test
+    @DisplayName("로그인 사용자 내정보 수정 - 실패(이름공백)")
+    @WithUser(value = "admin") //@BeforeEach 와 @WithUserDetails 사용시 오류로 인해 커스텀 태그 생성
+    void test_6() throws Exception{
+        String userId = "admin";
+        String newUserName = "";
+
+        mockMvc.perform(MockMvcRequestBuilders.post("/myPage/editMyInfo/{userId}", userId)
+                        .param("userId", userId)
+                        .param("userNm", newUserName))
+                .andExpect(status().is2xxSuccessful())
+                .andExpect(model().hasErrors())
+                .andExpect(model().attributeHasFieldErrorCode("userInfo", "userNm", "NotBlank"));
     }
 }
