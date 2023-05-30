ad1b42f4ef87864bddc4cbb53258a6b3cb465f80
pilming44 <pilming44@naver.com>
2023-05-30 21:52:55 +0900

    test : 사용하지않는 import 제거

--- src/test/java/study/toy/everythingshop/sample/SampleDAOTest.java
+++ src/test/java/study/toy/everythingshop/sample/SampleDAOTest.java
@@ -5,12 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.transaction.annotation.Transactional;
-import study.toy.everythingshop.entity.h2.SampleEntity;
 import study.toy.everythingshop.repository.SampleDAO;
-
-import java.util.List;
-
-import static org.assertj.core.api.Assertions.assertThat;
 
 @Slf4j
 @Transactional
