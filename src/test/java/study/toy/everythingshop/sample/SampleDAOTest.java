69f8d70a913585822a9163bf96177a1cf44fb44a
pilming44 <pilming44@naver.com>
2023-05-30 21:51:58 +0900

    test : 기존의 샘플로 만들어둔 테스트코드 삭제

--- src/test/java/study/toy/everythingshop/sample/SampleDAOTest.java
+++ src/test/java/study/toy/everythingshop/sample/SampleDAOTest.java
@@ -22,26 +22,7 @@ public class SampleDAOTest {
     @Test
     void 테스트명은한글도가능합니다() {
         /**
-         * 샘플 테스트로 간단하게 작성했습니다
-         * 실제로는 더 다양한 검증을 진행해야됩니다.
+         * 다양한 검증을 진행해야됩니다.
          */
-        //given
-        SampleEntity sampleEntity1 = new SampleEntity();
-        sampleEntity1.setName("최재현본체");
-
-        SampleEntity sampleEntity2 = new SampleEntity();
-        sampleEntity2.setName("최재현분신1");
-
-        SampleEntity sampleEntity3 = new SampleEntity();
-        sampleEntity3.setName("최재현분신2");
-
-        //when
-        sampleDAO.saveSample(sampleEntity1);
-        sampleDAO.saveSample(sampleEntity2);
-        sampleDAO.saveSample(sampleEntity3);
-
-        //then
-        List<SampleEntity> findAll = sampleDAO.findAll();
-        assertThat(findAll.size()).isEqualTo(3);
     }
 }
