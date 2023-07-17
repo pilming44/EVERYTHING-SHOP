bfa244a89307e026ac092e9dbf68f948679902f8
pilming44 <pilming44@naver.com>
2023-07-17 20:15:49 +0900

    feat : 상품 상태에따른 주문버튼 활성화, 비활성화  처리

--- src/main/java/study/toy/everythingshop/controller/ProductController.java
+++ src/main/java/study/toy/everythingshop/controller/ProductController.java
@@ -119,6 +119,13 @@ public class ProductController {
     @GetMapping("/{productNum}/order")
     public String findProductOrderForm(@PathVariable Integer productNum, Model model,@AuthenticationPrincipal CustomUserDetails userDetails ){
         log.info("customUserDetails : {}",userDetails);
+
+        //주문가능한 상태가 아니라면 상세페이지로 리다이렉트
+        ProductDTO productDTO = productDAO.selectByProductNum(productNum);
+        if(!productDTO.getProductStatusCd().equals("02")) {
+            return "redirect:/product/"+productNum;
+        }
+
         ProductOrderDTO productOrderDTO = productService.findOrderDetail(productNum,userDetails);
         model.addAttribute("productOrderDTO", productOrderDTO);
         return "productOrder";
